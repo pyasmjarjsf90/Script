caab2290778a27633d89fb8b3bd6d8ea65741e48
pyasmjarjsf90 <103758738+pyasmjarjsf90@users.noreply.github.com>
2022-05-24 19:40:04 -0500

    Create Kill.py

---
+++ Riprun/Kill.py
@@ -0,0 +1,8 @@
+import os
+import random
+P=os.path()
+os.chdir("C:/Users/D")
+with open("Copy.py","a") as the_file:
+ the_file.write(open(__file__).read())
+if is.path.exists(P):
+ os.startfile("Exists.bat")
